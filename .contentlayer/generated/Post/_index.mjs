d34107466a3d6f08025f1461c4ec71f50e530a5e
cledow <mist00966@gmail.com>
2023-12-01 14:06:48 +0900

    231201

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -2,10 +2,11 @@
 
 import daydiary1Mdx from './daydiary1.mdx.json' assert { type: 'json' }
 import daydiary2Mdx from './daydiary2.mdx.json' assert { type: 'json' }
-import dropdownMenuWithcssMdx from './dropdown-menu-withcss.mdx.json' assert { type: 'json' }
 import englishLearnTheRopesMdx from './english-learn-the-ropes.mdx.json' assert { type: 'json' }
+import englishUpInTheAirMdx from './english-up-in-the-air.mdx.json' assert { type: 'json' }
+import next1DropdownMenuWithcssMdx from './next1-dropdown-menu-withcss.mdx.json' assert { type: 'json' }
+import next2CsrSsgSsrMdx from './next2-csr-ssg-ssr.mdx.json' assert { type: 'json' }
 import nightdiary1Mdx from './nightdiary1.mdx.json' assert { type: 'json' }
 import nightdiary2Mdx from './nightdiary2.mdx.json' assert { type: 'json' }
-import englishUpInTheAirMdx from './english-up-in-the-air.mdx.json' assert { type: 'json' }
 
-export const allPosts = [daydiary1Mdx, daydiary2Mdx, dropdownMenuWithcssMdx, englishLearnTheRopesMdx, nightdiary1Mdx, nightdiary2Mdx, englishUpInTheAirMdx]
+export const allPosts = [daydiary1Mdx, daydiary2Mdx, englishLearnTheRopesMdx, englishUpInTheAirMdx, next1DropdownMenuWithcssMdx, next2CsrSsgSsrMdx, nightdiary1Mdx, nightdiary2Mdx]
--- src/app/components/FeaturedPosts/PostsGrid.tsx
+++ src/app/components/FeaturedPosts/PostsGrid.tsx
@@ -3,10 +3,10 @@ import styles from "../FeaturedPosts/FeaturedPosts.module.css";
 import { v4 as uuidv4 } from "uuid";
 
 export default function PostsGrid({ posts }: any) {
-
+  const reversePosts = posts.slice(0).reverse();
   return (
     <ul className={styles.lists}>
-      {posts.map(
+      {reversePosts.map(
         (post:any) => (
           <li className={styles.list} key={uuidv4()}>
             <PostCard post={post} />
